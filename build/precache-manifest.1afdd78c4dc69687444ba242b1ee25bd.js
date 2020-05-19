94d3937af6c22cd70aaa62ddec327b599664894a
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-18 22:24:43 -0700

    bugfixes

---
+++ build/precache-manifest.1afdd78c4dc69687444ba242b1ee25bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc7afa51129947fe0527f328887d32a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "14ab59563edd1b67b1c4",
+    "url": "/portfolio/static/css/main.f5bee342.chunk.css"
+  },
+  {
+    "revision": "d0561e8a0b11f7ac76ce",
+    "url": "/portfolio/static/js/2.41f70cce.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/portfolio/static/js/2.41f70cce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14ab59563edd1b67b1c4",
+    "url": "/portfolio/static/js/main.72aa1136.chunk.js"
+  },
+  {
+    "revision": "3372145aa467f5e49196",
+    "url": "/portfolio/static/js/runtime-main.1dc4ceff.js"
+  },
+  {
+    "revision": "5195f989f7e2d191ecc553870f135cac",
+    "url": "/portfolio/static/media/dpcc.5195f989.png"
+  }
+]);
